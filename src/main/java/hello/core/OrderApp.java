6c8fbca418556d579d70203a950860790cf0e822
withpaul <jongjin4749@gmail.com>
2023-01-10 23:23:36 +0900

    OCP, DIP를 잘지키기 위한 방법 - 순수 자바 코드로 컨테이너 비슷한거 만들어서 주입 - 스프링 컨테이너로도 해봄
    순수 자바로 짠 appconfig도 낫베드한데
    스프링 컨테이너는 얼마나 더강력할지 궁금하네

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,20 +3,24 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        OrderService orderService = appConfig.orderService();
-        Order itemA = orderService.createOrder(1L, "itemA", 10000);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        Order itemA = orderService.createOrder(1L, "itemA", 20000);
         System.out.println(itemA.toString());
         System.out.println(itemA.calculatePrice());
     }
