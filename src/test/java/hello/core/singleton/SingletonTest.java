d14deec433f969ade77f4b71b1f45b28cb4eb4ca
withpaul <jongjin4749@gmail.com>
2023-02-09 07:27:11 +0900

    스프링에서 제공하는 싱글톤이 진짜 같은지 테스트해보기

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,8 +1,11 @@
 package hello.core.singleton;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +19,18 @@ public class SingletonTest {
         SingletonService singletonService2 = SingletonService.getInstance();
 
         assertThat(singletonService1).isSameAs(singletonService2); // 주소값 비교
+    }
 
+    @Test
+    @DisplayName("싱글톤 스프링 사용하기")
+    void springSingleton()
+    {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean(MemberService.class);
+        MemberService memberService2 = ac.getBean(MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
     }
 }
